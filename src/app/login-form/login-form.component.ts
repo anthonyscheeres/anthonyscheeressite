8f9f19aafeb0531bdeea1812b2b8f0c58f11ce72
Anthony <info@anthonyscheeres.nl>
2020-03-23 12:29:46 +0100

    .

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { login } from '../services/user';
+import { responseR } from '../models/ResponseRequest';
+import { Router } from '@angular/router';
+import { DataModel } from '../models/DataModel';
+import { PermissionModel } from '../models/PermissionModel';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginFormComponent implements OnInit {
+ 
+
+
+  constructor(private _router: Router) { }
+
+  this1 = "";
+  ngOnInit() {
+  }
+
+
+  /**
+  *
+  * @author Anthony Scheeres
+  *
+  */
+  async loginUser(event) {
+
+    event.preventDefault()
+    const target = event.target
+
+    const username = target.querySelector('#username').value
+    const password = target.querySelector('#password').value
+   
+
+
+    await login(username, password).then(response => {
+      console.log(response);
+
+      if (response == '"fail"') {
+        this.this1 = "Oops think you entered invalid credentials, maybe you haven't verified your email yet?!"
+
+      }
+
+      else  {
+        DataModel.account;
+        var jsonObject = JSON.parse(response);
+        localStorage.setItem("token", jsonObject)
+
+      //  console.log(jsonObject);
+
+        DataModel.account = jsonObject;
+
+       // console.log(DataModel.account[2]);
+        this._router.navigate(['/']);
+      }
+      
+    })
+    ;
+
+
+
+
+
+
+
+  };
+}
