48b0aa9b4b165bd72b019eaef4ae5a9393db7ee9
Anthony <info@anthonyscheeres.nl>
2021-08-14 16:48:12 +0200

    updated the website to complement my CV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ContactComponent } from './contact/contact.component';
+
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { LoginComponent } from './login/login.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './header/header.component';
 import { ServicesInformationComponent } from './services-information/services-information.component';
 import { PortefolioComponent } from './portefolio/portefolio.component';
-
-
-
-
 
 
 @NgModule({
@@ -26,13 +20,11 @@ import { PortefolioComponent } from './portefolio/portefolio.component';
 
     AppComponent,
     NavbarComponent,
-    ContactComponent,
+  
 
     HomeComponent,
     FooterComponent,
-    ProfileComponent,
-    LoginFormComponent,
-    LoginComponent,
+  
 
     HeaderComponent,
     ServicesInformationComponent,
@@ -59,24 +51,11 @@ import { PortefolioComponent } from './portefolio/portefolio.component';
           path: '',
           redirectTo: '/home', pathMatch: 'full'
         },
+      
         {
-          path: 'profile',
-          component: ProfileComponent
-        },
-
-        {
-          path: 'contact',
-          component: ContactComponent
-        },
- {
           path: 'home',
           component: HomeComponent
         },
-        {
-          path: 'login',
-          component: LoginComponent
-        },
-
 
         // otherwise redirect to home
         { path: '**', redirectTo: '' }
